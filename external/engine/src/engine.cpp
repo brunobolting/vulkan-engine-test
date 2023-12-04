14a94f198f954935e1db8052a96831a641737b97
Bruno Bolting <brunoboltingx@gmail.com>
2023-12-04 19:25:29 -0300

    feat: initial cmake configuration

---
+++ external/engine/src/engine.cpp
@@ -0,0 +1,7 @@
+#include <engine/engine.h>
+#include <iostream>
+
+void Engine::PrintHelloWorld()
+{
+    std::cout << "Hello, I'm talking to you from the engine library" << std::endl;
+}
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <engine/engine.h>
+
+int main(int argc, char** argv) {
+  std::cout << "Hello, world!" << std::endl;
+  Engine::PrintHelloWorld();
+  return 0;
+}
